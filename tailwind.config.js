a6480996e48b025bda0d70b84e28be66ccca0335
Trần Nam <namtran96hth@gmail.com>
2024-12-14 00:03:20 +0700

    Config Tailwind and Jit Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        success: "#40C057",
+      },
+    },
   },
   plugins: [],
 };
