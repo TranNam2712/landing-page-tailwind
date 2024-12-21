a58000e3e1dd0b1a7dd8264ae27c585835453418
Trần Nam <namtran96hth@gmail.com>
2024-12-21 15:46:33 +0700

    Landing page part 3: Create Story

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         primary: "#FF6363",
         success: "#40C057",
+        orange: "#FFB700",
+        "black-orange": "#FF6A00",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
